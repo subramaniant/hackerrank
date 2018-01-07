8cbb122072aa5b36d99c31cba9ed048dac2c991d
subramanian <subramanian@192.168.1.8>
2018-01-07 14:28:48 +0530

    done

--- hackerrank/src/com/hackerrank/Staircase.java
+++ hackerrank/src/com/hackerrank/Staircase.java
@@ -1,10 +1,33 @@
 package com.hackerrank;
 
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/staircase/problem
+ * @author subramanian
+ *
+ */
 public class Staircase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void staircase(int n) {
+		for(int i=1;i<=n;i++) {
+			int rem = n-i;
+			for(int k=1;k<=rem;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        staircase(n);
+        in.close();
 	}
 
 }
